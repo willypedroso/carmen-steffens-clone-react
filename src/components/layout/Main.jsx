9fd1c98463671711b94edebc3cc928a80c269495
Will Pedroso <willyng.pedroso@gmail.com>
2022-11-12 00:47:50 -0300

    add product description for bigger rendering

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import productPic1 from '../../images/product-pic.jpeg';
 import productPic2 from '../../images/product-pic2.jpeg';
+import { HiGift } from "react-icons/hi";
 
 function Main(props) {
     const [mainPicture, setMainPicture] = useState(productPic1);
@@ -32,9 +33,15 @@ function Main(props) {
 
                 {/* Product details */}
                 <div className='flex flex-col m-10 w-[350px]'>
+
+                    {/* Product name */}
                     <h3>Flat Alfinete CS</h3>
                     <p className='text-gray-600 text-[0.6rem] mt-1'>Ref.: 0204.463.001A</p>
+
+                    {/* Product price */}
                     <h3 className='mt-2 font-medium'>R$349,90</h3>
+
+                    {/* Product sizes */}
                     <div className='flex flex-wrap mt-2'>
                         <span className='m-1 mr-2 ml-0 pt-[1px] pb-[1px] p-[12px] cursor-pointer
                             text-[0.88rem] border-2 border-gray-300 text-gray-300 rounded-md font-bold'
@@ -73,6 +80,7 @@ function Main(props) {
                     
                     <p className='mt-2 text-[0.8rem] cursor-pointer'><u>Veja nosso <strong>guia de medidas</strong></u></p>
 
+                    {/* Buttons */}
                     {/* Buy button */}
                     <div className='flex justify-center bg-[#a9cf80] text-white
                         p-3 text-[0.78rem] font-bold mt-3 cursor-pointer'>
@@ -80,9 +88,30 @@ function Main(props) {
                     </div>
                     
                     {/* Add to cart button */}
-                    <div className='flex justify-center bg-white text-[#a9cf80] border-4 border-[#a9cf80]
+                    <div className='flex justify-center bg-white text-[#a9cf80] border-[3px] border-[#a9cf80]
                         p-3 text-[0.78rem] font-bold mt-2 cursor-pointer'>
                             <h1>ADICIONAR AO CARRINHO</h1>
+                    </div>
+                    
+                    {/* Ask for present button */}
+                    <div className='flex justify-center bg-[#eac26a] text-black
+                        p-2 text-[0.78rem] font-bold mt-2 cursor-pointer'>
+                            <HiGift className='inline text-[1.3rem] mr-3'/><h1>PEDIR DE PRESENTE</h1>
+                    </div>
+
+                    {/* Product description */}
+                    <div className='text-[0.7rem]'>
+                        <p className='mt-2'><strong>Descrição</strong></p>
+                        <p className='mt-3'>Rasteira de dedo e de bico quadrado confeccionada em couro legítimo. Com detalhe de alfinete personalizado com logo CS, este modelo conta com fechamento com fivela.</p>
+                        
+                        <p className='mt-3'><strong>Por que apostar:</strong></p>
+                        <p className='mt-3'>Minimalista, esta flat na cor preta acompanha os looks do dia a dia com muito conforto. Combine com vestido para compor uma produção fresh ou vá de short de alfaiataria e blusa leve para criar uma inspiração elegante.</p>
+
+                        <p className='mt-3'><strong>Material:</strong> Couro legítimo</p>
+
+                        <p className='mt-3'><strong>Cor:</strong> Preto</p>
+
+                        <p className='mt-3'><strong>Salto:</strong> 1 cm</p>
                     </div>
                 </div>
             </div>
