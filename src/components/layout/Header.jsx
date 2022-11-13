49f6cb8357f6b00a29e895801ba1f98b08742de8
Will Pedroso <willyng.pedroso@gmail.com>
2022-11-12 21:06:50 -0300

    add data

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,6 +4,8 @@ import whiteLogoCsc from '../../images/icon-brand-white-csc.png';
 import whiteLogoCsy from '../../images/icon-brand-white-csy.png';
 import logoCinza from '../../images/logo-cinza.svg';
 
+import { menuSapatos, menuBolsas, menuRoupas, menuCsYoung, menuAcessorios, menuOutlet } from '../../data/menus';
+
 import { RiArrowDropDownFill } from "react-icons/ri";
 import { HiMenu, HiOutlineUser, HiOutlineShoppingBag, HiSearch } from "react-icons/hi";
 import { useState } from 'react';
@@ -148,22 +150,9 @@ function Header(props) {
                                 <div className='flex flex-col flex-wrap absolute bg-white shadow-lg top-[160px] h-[140px] w-[800px] p-[20px]'
                                 onMouseOver={() => setRenderSapatos(true)}
                                 onMouseOut={() => setRenderSapatos(false)}>
-                                    <a href="#"><strong>VER TODOS</strong></a>
-                                    <a href="#"><strong>FLATS</strong></a>
-                                    <a href="#"><strong>PLATAFORMAS</strong></a>
-                                    <a href="#"><strong>TAMANCOS</strong></a>
-                                    <a href="#"><strong>ANABELAS</strong></a>
-                                    <a href="#"><strong>MOCASSINS</strong></a>
-                                    <a href="#"><strong>SANDÁLIAS</strong></a>
-                                    <a href="#"><strong>TÊNIS</strong></a>
-                                    <a href="#"><strong>BOTAS</strong></a>
-                                    <a href="#"><strong>MULES</strong></a>
-                                    <a href="#"><strong>SAPATILHAS</strong></a>
-                                    <a href="#"><strong>LINHA CONFORT</strong></a>
-                                    <a href="#"><strong>CHINELOS</strong></a>
-                                    <a href="#"><strong>PEEP TOES</strong></a>
-                                    <a href="#"><strong>SCARPINS</strong></a>
-                                    <a href="#"><strong>LINHA NOVA</strong></a>
+                                    {menuSapatos.map(item => (
+                                        <a key={item} href="#"><strong>{item}</strong></a>
+                                    ))}                                  
                                 </div>
                             ) : false }
                             <ul className='cursor-pointer'
@@ -176,17 +165,9 @@ function Header(props) {
                                 <div className='flex flex-col flex-wrap absolute bg-white shadow-lg top-[160px] h-[140px] w-[800px] p-[20px]'
                                 onMouseOver={() => setRenderBolsas(true)}
                                 onMouseOut={() => setRenderBolsas(false)}>
-                                    <a href="#"><strong>VER TODOS</strong></a>
-                                    <a href="#"><strong>BOLSAS DE OMBRO</strong></a>
-                                    <a href="#"><strong>MOCHILAS</strong></a>
-                                    <a href="#"><strong>BOLSAS DE MÃO</strong></a>
-                                    <a href="#"><strong>BOLSAS TRANSVERSAIS</strong></a>
-                                    <a href="#"><strong>MALAS</strong></a>
-                                    <a href="#"><strong>BOLSAS DE MÃO E DE OMBRO</strong></a>
-                                    <a href="#"><strong>BOLSAS PARA LAPTOP</strong></a>
-                                    <a href="#"><strong>POCHETES</strong></a>
-                                    <a href="#"><strong>BOLSAS DE MÃO E TRANSVERSAIS</strong></a>
-                                    <a href="#"><strong>CLUTCHES</strong></a>
+                                    {menuBolsas.map(item => (
+                                        <a href="#"><strong>{item}</strong></a>
+                                    ))}
                                 </div>
                             ) : false }
                             <ul className='cursor-pointer'
@@ -199,19 +180,9 @@ function Header(props) {
                                 <div className='flex flex-col flex-wrap absolute bg-white shadow-lg top-[160px] h-[140px] w-[800px] p-[20px]'
                                 onMouseOver={() => setRenderRoupas(true)}
                                 onMouseOut={() => setRenderRoupas(false)}>
-                                    <a href="#"><strong>VER TODOS</strong></a>
-                                    <a href="#"><strong>CAMISAS</strong></a>
-                                    <a href="#"><strong>SAIAS</strong></a>
-                                    <a href="#"><strong>BEACHWEAR</strong></a>
-                                    <a href="#"><strong>BLUSAS E TOPS</strong></a>
-                                    <a href="#"><strong>CASACOS E JAQUETAS</strong></a>
-                                    <a href="#"><strong>SHORTS</strong></a>
-                                    <a href="#"><strong>BODY</strong></a>
-                                    <a href="#"><strong>LENÇOS</strong></a>
-                                    <a href="#"><strong>VESTIDOS</strong></a>
-                                    <a href="#"><strong>CALÇAS</strong></a>
-                                    <a href="#"><strong>MACACÕES</strong></a>
-                                    <a href="#"><strong>LINGERIE</strong></a>
+                                    {menuRoupas.map(item => (
+                                        <a href="#"><strong>{item}</strong></a>
+                                    ))}
                                 </div>
                             ) : false }
                             <ul className='cursor-pointer'
@@ -221,13 +192,12 @@ function Header(props) {
                 
                             {/* Conditional submenu "CS Young" */}
                             {renderCsyoung ? (
-                                <div className='flex justify-between flex-wrap absolute bg-white shadow-lg top-[160px] h-[140px] w-[800px] p-[20px]'
+                                <div className='flex justify-between flex-wrap absolute bg-white shadow-lg top-[160px] h-[60px] w-[800px] p-[20px]'
                                 onMouseOver={() => setRenderCsyoung(true)}
                                 onMouseOut={() => setRenderCsyoung(false)}>
-                                    <a href="#"><strong>CS YOUNG ROUPAS</strong></a>
-                                    <a href="#"><strong>CS YOUNG BOLSAS</strong></a>
-                                    <a href="#"><strong>CS YOUNG SAPATOS</strong></a>
-                                    <a href="#"><strong>CS YOUNG ACESSÓRIOS</strong></a>
+                                    {menuCsYoung.map(item => (
+                                        <a href="#"><strong>{item}</strong></a>
+                                    ))}
                                 </div>
                             ) : false }
                             <ul className='cursor-pointer'
@@ -240,19 +210,9 @@ function Header(props) {
                                 <div className='flex flex-col justify-between flex-wrap absolute bg-white shadow-lg top-[160px] h-[140px] w-[800px] p-[20px]'
                                 onMouseOver={() => setRenderAcessorios(true)}
                                 onMouseOut={() => setRenderAcessorios(false)}>
-                                    <a href="#"><strong>VER TODOS</strong></a>
-                                    <a href="#"><strong>CARTEIRAS</strong></a>
-                                    <a href="#"><strong>ITENS ESPECIAIS</strong></a>
-                                    <a href="#"><strong>RELÓGIOS</strong></a>
-                                    <a href="#"><strong>ANÉIS</strong></a>
-                                    <a href="#"><strong>CINTOS</strong></a>
-                                    <a href="#"><strong>ÓCULOS</strong></a>
-                                    <a href="#"><strong>BONÉS</strong></a>
-                                    <a href="#"><strong>COLARES</strong></a>
-                                    <a href="#"><strong>PORTA-CELULAR</strong></a>
-                                    <a href="#"><strong>BRINCOS</strong></a>
-                                    <a href="#"><strong>CHAVEIROS</strong></a>
-                                    <a href="#"><strong>PULSEIRAS</strong></a>
+                                    {menuAcessorios.map(item => (
+                                        <a href="#"><strong>{item}</strong></a>
+                                    ))}
                                 </div>
                             ) : false }
                             <ul className='cursor-pointer'
@@ -265,14 +225,12 @@ function Header(props) {
                                 <div className='flex flex-col justify-between flex-wrap absolute bg-white shadow-lg top-[160px] h-[140px] w-[800px] p-[20px]'
                                 onMouseOver={() => setRenderOutlet(true)}
                                 onMouseOut={() => setRenderOutlet(false)}>
-                                    <a href="#"><strong>SAPATOS</strong></a>
-                                    <a href="#"><strong>BOLSAS</strong></a>
-                                    <a href="#"><strong>ROUPAS</strong></a>
-                                    <a href="#"><strong>ACESSÓRIOS</strong></a>
-                                    <a href="#"><strong>CS YOUG BOLSAS</strong></a>
-                                    <a href="#"><strong>CS YOUNG ROUPAS</strong></a>
-                                    <a href="#"><strong>CS YOUNG SAPATOS</strong></a>
-                                    <a href="#"><strong>CS YOUNG ACESSÓRIOS</strong></a>
+                                    {menuOutlet.map(item => (
+                                        <a href="#"><strong>{item}</strong></a>
+                                    ))}
+                                    {menuCsYoung.map(item => (
+                                        <a href="#"><strong>{item}</strong></a>
+                                    ))}
                                 </div>
                             ) : false }
                         </div>
---
+++ src/data/menus.js
@@ -0,0 +1,87 @@
+const menuSapatos = [
+    "VER TODOS",
+    "FLATS",
+    "PLATAFORMAS",
+    "TAMANCOS",
+    "ANABELAS",
+    "MOCASSINS",
+    "SANDÁLIAS",
+    "TÊNIS",
+    "BOTAS",
+    "MULES",
+    "SAPATILHAS",
+    "LINHA CONFORT",
+    "CHINELOS",
+    "PEEP TOES",
+    "SCARPINS",
+    "LINHA VOVA"
+];
+
+const menuBolsas = [
+    "VER TODOS",
+    "BOLSAS DE OMBRO",
+    "MOCHILAS",
+    "BOLSAS DE MÃO",
+    "BOLSAS TRANSVERSAIS",
+    "MALAS",
+    "BOLSAS DE MÃO E DE OMBRO",
+    "BOLSAS PARA LAPTOP",
+    "POCHETES",
+    "BOLSAS DE MÃO E TRANSVERSAIS",
+    "CLUTCHES"
+];
+
+const menuRoupas = [
+    "VER TODOS",
+    "CAMISAS",
+    "SAIAS",
+    "BEACHWEAR",
+    "BLUSAS E TOPS",
+    "CASACOS E JAQUETAS",
+    "SHORTS",
+    "BODY",
+    "LENÇOS",
+    "VESTIDOS",
+    "CALÇAS",
+    "MACACÕES",
+    "LINGERIE"
+];
+
+const menuCsYoung = [
+    "CS YOUNG ROUPAS",
+    "CS YOUNG BOLSAS",
+    "CS YOUNG SAPATOS",
+    "CS YOUNG ACESSÓRIOS"
+];
+
+const menuAcessorios = [
+    "VER TODOS",
+    "CARTEIRAS",
+    "ITENS ESPECIAIS",
+    "RELÓGIOS",
+    "ANÉIS",
+    "CINTOS",
+    "ÓCULOS",
+    "BONÉS",
+    "COLARES",
+    "PORTA-CELULAR",
+    "BRINCOS",
+    "CHAVEIROS",
+    "PULSEIRAS"
+];
+
+const menuOutlet = [
+    "SAPATOS",
+    "BOLSAS",
+    "ROUPAS",
+    "ACESSÓRIOS"
+];
+
+export {
+    menuSapatos,
+    menuBolsas,
+    menuRoupas,
+    menuCsYoung,
+    menuAcessorios,
+    menuOutlet
+};
